7faf1265bfce7e61a60b6e218fd3b54a6d5aa5a1
Achraf <achrafelkhamsi@gmail.com>
2025-04-22 22:11:07 +0200

    update faq page for summer camp

--- apps/front/app/(pages)/(static)/faq/page.tsx
+++ apps/front/app/(pages)/(static)/faq/page.tsx
@@ -1,6 +1,6 @@
 import TextureBg from "@/app/components/texture-bg"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@mdm/ui"
-import { generalQuestions, mathSprintQuestions, bestMathVideoQuestions, standQuestions, conferencesQuestions } from "./questions"
+import { generalQuestions } from "./questions"
 import { FaqAccordion } from "./faq-accordion"
 
 
@@ -24,7 +24,7 @@ export default function ConferencesPage() {
           className="animate-fade-up bg-clip-text text-center font-display opacity-0"
           style={{ animationDelay: "0.20s", animationFillMode: "forwards" }}
         >
-          Pour toute autre question non traitée sur le site vous pouvez nous contacter via nos réseaux sociaux ou par email sur <span className="text-blue-500">mathmarocmdm@gmail.com</span><br/>
+          Pour toute autre question non traitée sur le site vous pouvez nous contacter via nos réseaux sociaux ou par email sur <span className="text-blue-500">math.maroc.summer.camp@gmail.com</span><br/>
           Notez cependant que nous ne pouvons répondre qu&apos;aux questions non discutées sur le site.
         </p>
 
@@ -32,37 +32,7 @@ export default function ConferencesPage() {
           className="animate-fade-up opacity-0"
           style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
         >
-          <Tabs defaultValue="general">
-            <TabsList className="grid grid-cols-1 md:grid-cols-3">
-              <TabsTrigger value="general" className="text-sm">Général</TabsTrigger>
-              <TabsTrigger value="math_sprint">Math Sprint</TabsTrigger>
-              <TabsTrigger value="best_math_video">Best Math Video</TabsTrigger>
-              <TabsTrigger value="stand">Stand</TabsTrigger>
-              {/* <TabsTrigger value="conferences">Conférences</TabsTrigger> */}
-            </TabsList>
-
-            <div className="mt-18 md:mt-0">
-              <TabsContent value="general">
-                <FaqAccordion items={generalQuestions} />
-              </TabsContent>
-
-              <TabsContent value="math_sprint">
-                <FaqAccordion items={mathSprintQuestions} />
-              </TabsContent>
-
-              <TabsContent value="best_math_video">
-                <FaqAccordion items={bestMathVideoQuestions} />
-              </TabsContent>
-
-              <TabsContent value="stand">
-                <FaqAccordion items={standQuestions} />
-              </TabsContent>
-
-              {/* <TabsContent value="conferences">
-                <FaqAccordion items={conferencesQuestions} />
-              </TabsContent> */}
-            </div>
-          </Tabs>
+          <FaqAccordion items={generalQuestions} />
         </div>
       </div>
     </TextureBg>
--- apps/front/app/(pages)/(static)/faq/questions.ts
+++ apps/front/app/(pages)/(static)/faq/questions.ts
@@ -1,183 +1,38 @@
 export const generalQuestions = [
   {
-    question: "Combien de temps durera l'événement ?",
-    anwser: "L'événement durera une journée. Le programme complet de cette journée sera publié ultérieurement."
+    question: "Qui peut participer ?",
+    anwser: "Les élèves inscrits en dernière année de collège et en première année de lycée pendant l'année scolaire 2024-2025 peuvent participer à cette édition de Summer Camp."
   },
   {
-    question: "Qui peut s'inscrire ?",
-    anwser: "Toute personne intéressée par les mathématiques peut participer à cet événement."
+    question: "Quelle est la date limite de candidature ?",
+    anwser: "La date limite est le 21 juin à 23h59, heure marocaine. Ne tardez pas à soumettre votre candidature. Aucune exception ne sera accordée pour les soumissions tardives, quelles que soient les raisons, qu'il s'agisse d'un oubli, de problèmes de connexion Internet ou de toute autre excuse."
   },
   {
-    question: "Quel est le délai d’inscription ?",
-    anwser: "La date limite pour les visiteurs est fixée au 10 avril 2025. Pour la participation aux compétitions ou à l'animation des stands, vous trouverez les dates limites sur leurs pages correspondantes. N'attendez pas jusqu'au dernier jour pour soumettre votre candidature. Aucune exception ne sera accordée pour les candidatures tardives, quelles que soient les raisons invoquées, telles que l'oubli, des problèmes de connexion internet ou toute autre excuse."
+    question: "Est-ce que la participation à cet événement est gratuite ?",
+    anwser: "La participation à cet événement est gratuite. Elle inclut l’hébergement et les repas à partir du samedi 20 juillet au soir jusqu’au dimanche 28 juillet au matin."
   },
   {
-    question: "Est-ce que la nourriture et le transport seront pris en charge ?",
-    anwser: "Non, tout le transport et la nourriture seront entièrement à votre charge. En cas d’exception, vous en serez informés."
+    question: "Si j’ai une question, que dois-je faire ?",
+    anwser: "Écrivez à math.maroc.summer.camp@gmail.com. Toute réponse fournie par un autre moyen ou par une autre personne n'est pas considérée comme officielle."
   },
   {
-    question: "Est-ce que nos parents peuvent nous accompagner pendant l’événement ?",
-    anwser: "Oui, vous pouvez être accompagnés par vos parents pendant l’événement."
+    question: "Si je n’ai pas les bulletins de cette année, que dois-je faire ?",
+    anwser: "Tous les documents sont requis et seront vérifiés individuellement par notre équipe. Si vous n'avez pas encore obtenu votre relevé de notes officiel, vous devez profiter de la période d'inscription que nous offrons pour le demander à votre école. Dans ce cas, nous vous recommandons d'attendre d'avoir tous vos documents avant de soumettre votre candidature (évitez cependant d'attendre le dernier jour). Si vous ne l'avez toujours pas reçu le 14 Juin, veuillez nous envoyer un mail à ce moment-là pour expliquer la situation en détail."
   },
   {
-    question: "Où pourrais-je vous contacter si j’ai une demande spéciale, ou une autre question ?",
-    anwser: "Vous pouvez envoyer un email à la boîte mail suivante: mathmarocmdm@gmail.com"
+    question: "Comment faut-il signer le document du règlement ?",
+    anwser: "L'élève et le tuteur légal doivent signer le règlement: il faut imprimer le document, le remplir et le signer à la main, puis le scanner. Cependant, il n’est pas nécessaire de le légaliser."
   },
   {
-    question: "Pourrais-je participer même si je n’ai pas encore de carte d’identité nationale ?",
-    anwser: "Oui, il suffit de mettre une preuve d’identité qui contient une photo; exemples: attestation de scolarité, carte étudiant, carte de club, carte de fondation…"
+    question: "Comment faut-il signer le document de l’autorisation parentale ?",
+    anwser: "Le parent/tuteur légal doit signer le document d’autorisation parentale: il faut imprimer le document, le remplir et le signer à la main, puis le scanner. Ce document doit être légalisé."
   },
   {
-    question: "Faut-il une lettre de motivation lors de l’inscription ?",
-    anwser: "Non, les lettres de motivation ne seront pas prises en considération. Par contre dans le formulaire d’inscription il sera demandé d’inclure une motivation de max 300 mots pour les participants aux compétitions ou aux animateurs de stands."
+    question: "Quand est-ce que vous allez répondre à ma candidature ?",
+    anwser: "Les résultats seront annoncés quelques semaines après la date limite de dépôt des candidatures. Chaque candidat, qu'il soit sélectionné, mis en liste d'attente ou non sélectionné, recevra une réponse. Nous vous prions de ne pas envoyer de mail à ce sujet."
   },
   {
-    question: "Que faire si je n’ai pas encore reçu mon relevé de notes ?",
-    anwser: "Normalement, vous avez tous le bulletin du premier semestre de l'année en cours. Si vous l’aviez perdu, il est de votre responsabilité de solliciter votre école pour une copie. En cas de preuve sérieuse on peut accepter le bulletin de l'année précédente."
+    question: "Quand puis-je espérer recevoir une réponse définitive si je suis sur la liste d'attente ?",
+    anwser: "Nous contacterons les candidats sur liste d'attente individuellement et progressivement au fur et à mesure que des places se libèrent. La libération d'une place peut se produire jusqu'à quelques jours avant l'événement, donc nous ne pouvons pas fournir de calendrier précis. Nous vous prions de ne pas envoyer de mails concernant votre position sur la liste d'attente, car nous ne répondrons pas à ceux-ci."
   },
-  {
-    question: "Quelle est la langue utilisée pour les conférences?",
-    anwser: "Les conférences sont en français."
-  },
-]
-
-
-export const mathSprintQuestions = [
-  {
-    question: "Comment savoir si j’ai été sélectionné",
-    anwser: "Vous serez notifié par email et on vous ajoutera dans un groupe whatsapp, pensez à bien vérifier que vous avez mis le bon numéro de téléphone qui contient whatsapp."
-  },
-  {
-    question: "Quand est-ce qu’on sera notifié si on est sélectionné ?",
-    anwser: "Les résultats de sélection seront envoyés au plus tard mi-mars."
-  },
-  {
-    question: "Est-ce qu’il y aura des problèmes différents selon le niveau scolaire ?",
-    anwser: "Non, tous les candidats auront affaire aux mêmes problèmes. il y aura juste deux types d’épreuves : une pour le collège et l’autre pour le lycée."
-  },
-  {
-    question: "Y-a-t-il des problèmes sur lesquels on pourra s'entraîner ?",
-    anwser: "Vous trouverez des problèmes d'entraînement dans la page dédiée à Math Sprint."
-  },
-  {
-    question: "Est-ce qu’il y aura des certificats de participation ?",
-    anwser: "Oui, il y aura des certificats aux gagnants ainsi qu’aux participants."
-  },
-  {
-    question: "Quels sont les prix à gagner ?",
-    anwser: "Les prix seront annoncés avant la journée de MDM sur nos réseaux sociaux."
-  },
-]
-
-export const bestMathVideoQuestions = [
-  {
-    question: "Qui peut s’inscrire ?",
-    anwser: "La compétition “Best Mathematical Video” est ouverte à tous les élèves de moins de 18 ans, du primaire, du collège et du lycée."
-  },
-  {
-    question: "Puis-je participer tout en étant candidat à d’autres compétitions de MDM (ex: Math sprint)",
-    anwser: "Oui tout à fait."
-  },
-  {
-    question: "Puis-je participer en équipe ?",
-    anwser: "Non, les participations sont individuelles."
-  },
-  {
-    question: "Puis-je soumettre plusieurs vidéos ?",
-    anwser: "Non. Chaque participation est unique. Toute autre soumission sera automatiquement rejetée."
-  },
-  {
-    question: "Y a-t-il une durée limite pour la vidéo ?",
-    anwser: "La vidéo doit durer au maximum 4 minutes et au minimum 3 minutes. Au-delà de ceci la vidéo sera sanctionnée."
-  },
-  {
-    question: "Je ne souhaite pas montrer mon visage sur la vidéo; Est-ce un problème?",
-    anwser: "Non. Si vous n’êtes pas à l’aise à le faire, ne vous inquiétez pas ce n’est pas obligatoire (mais encouragé si vous le pouvez). Il est cependant nécessaire que vous utilisiez votre propre voix pour expliquer et non une IA."
-  },
-  {
-    question: "Puis-je obtenir de l’aide d’un adulte pour préparer ma vidéo ?",
-    anwser: "Oui avec modération. N’oubliez pas que le travail doit être fait par vous."
-  },
-  {
-    question: "Je ne sais pas comment faire le montage, serais-je pénalisé?",
-    anwser: "Ne vous inquiétez pas! On ne juge pas vos aptitudes de montage mais plutôt le contenu de la vidéo. Il suffit que la vidéo soit cohérente et présentable. Vous trouverez par ailleurs une liste de logiciels de montage pour vous aider à démarrer sur la page de Best Math video. Vous n’êtes bien entendu pas obligés de les utiliser."
-  },
-  {
-    question: "Puis-je inclure des graphiques, animations ou effets spéciaux dans ma vidéo ?",
-    anwser: "Oui. Nous vous encourageons vivement à le faire. Le but est de rendre la vidéo aussi ludique que possible."
-  },
-  {
-    question: "Puis-je choisir n’importe quel sujet?",
-    anwser: "Oui. L’idée de la compétition est que vous expliquiez un théorème ou un concept mathématique de la manière la plus abordable. Nous ne recherchons pas un cours classique ni une simple narration historique, mais une vraie explication du concept lui-même."
-  },
-  {
-    question: "Les vidéos seront-elles partagées publiquement ou resteront-elles privées ?",
-    anwser: "Les vidéos seront partagées publiquement sur Youtube. En signant le règlement de MDM, vous consentez à ceci."
-  },
-  {
-    question: "Quand et comment les gagnants seront-ils annoncés ?",
-    anwser: "Les gagnants seront contactés par email brièvement après l’évaluation des participations."
-  },
-  {
-    question: "Si je suis parmi les gagnants, suis-je obligé d’assister à la journée MDM ?",
-    anwser: "Oui. En signant le règlement, tu t'engages à te présenter à MDM si ta vidéo a été sélectionnée. En cas d'empêchement majeur, contactez nous."
-  },
-  {
-    question: "Quels sont les prix à gagner ?",
-    anwser: "4 gagnants seront choisi comme suit: 3 prix du jury math et maroc et 1 prix choix du public"
-  },
-  {
-    question: "Est-ce qu'un certificat de participation sera remis à tous les participants ?",
-    anwser: "Tous les participants peuvent recevoir une attestation de participation sur demande. Veuillez nous contacter sur mathmarocmdm@gmail.com."
-  },
-]
-
-export const standQuestions = [
-  {
-    question: "Comment puis-je m’inscrire pour tenir un stand ?",
-    anwser: "L’inscription se fait en ligne via le site web officiel du MDM  avant la date limite."
-  },
-  {
-    question: "Y a-t-il des frais d’inscription pour proposer un stand ?",
-    anwser: "Non, la participation est gratuite pour tous les animateurs souhaitant exposer leur stand."
-  },
-  {
-    question: "Quelle est la durée de l'exposition des stands ?",
-    anwser: "Les stands seront accessibles au public pendant une grande partie de la journée du MDM. L’objectif est de permettre aux visiteurs de circuler librement entre les stands pour en découvrir plusieurs."
-  },
-  {
-    question: "Puis-je présenter mon stand avec un groupe ?",
-    anwser: "Oui, il est possible d’animer un stand en équipe."
-  },
-  {
-    question: "Quel matériel sera mis à disposition pour les stands ?",
-    anwser: "Les organisateurs fourniront des modulaires (stands non personnalisés). Pour tout matériel supplémentaire (écrans, affiches, maquettes, etc.), les participants devront s’en occuper eux-mêmes."
-  },
-  {
-    question: "Puis-je proposer un stand si je suis un étudiant indépendant ?",
-    anwser: "Les stands sont principalement destinés aux clubs, associations et entreprises. Cependant, si vous avez un projet solide et une bonne capacité de vulgarisation, vous pouvez candidater pour étudier la faisabilité de votre participation."
-  },
-  {
-    question: "Que se passe-t-il si je dois annuler ma participation ?",
-    anwser: "Si vous ne pouvez plus participer, veuillez prévenir les organisateurs dès que possible pour libérer votre place et permettre à d’autres participants de s’inscrire."
-  },
-  {
-    question: "Les frais de transports seront-ils remboursés ?",
-    anwser: "Oui, pour deux personnes au maximum pour chaque stand. Les frais de trains."
-  },
-  {
-    question: "Le logement des animateurs et leur restauration seront t-ils assurés par Math&Maroc ?",
-    anwser: "Nous avons la capacité de loger deux à trois personnes par stand la nuit du 18 au 19 juin. La restauration est offerte à l’ensemble des animateurs pour le dîner du 18, le petit déjeuner et le déjeuner du 19. "
-  },
-]
-
-export const conferencesQuestions = [
-  // {
-  //   question: "Question 7",
-  //   anwser: "Anwser 7"
-  // },
-  // {
-  //   question: "Question 8",
-  //   anwser: "Anwser 8"
-  // },
 ]
