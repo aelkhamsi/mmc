a1a83acb7882af7988e79a21a361f3209b3a67bf
Achraf <achrafelkhamsi@gmail.com>
2025-05-31 01:08:50 +0200

    change metadata title for admin website

--- apps/admin/src/app/layout.tsx
+++ apps/admin/src/app/layout.tsx
@@ -7,7 +7,7 @@ import { DataLoader } from '@/components/layout/data-loader';
 import { Toaster } from '@/components/shared/toaster';
 
 export const metadata: Metadata = {
-  title: "Summer Camp 2025 Admin",
+  title: "MMC 2025 Admin",
 };
 
 export default function RootLayout({
